a4410fd0a3fcd3ca88abcc4a4209cdfd230984bc
Robins Gupta <robinskumar73@gmail.com>
2015-11-06 15:21:26 +0530

    First commit of the template

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name templateAdminApp
+ * @description
+ * # templateAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('templateAdminApp', [
+    'ngAnimate',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize'
+  ]);
